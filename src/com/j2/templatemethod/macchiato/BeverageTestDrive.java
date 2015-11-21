1107eb87e3169e9f1c921fdaf3ed58b8929bbd08
pce913 <pce00913@naver.com>
2015-11-21 22:22:48 +0900

    caramelMacchiato templatemethod

---
+++ src/com/j2/templatemethod/macchiato/BeverageTestDrive.java
@@ -0,0 +1,9 @@
+package com.j2.templatemethod.macchiato;
+
+public class BeverageTestDrive {
+	public static void main(String[] args) {
+	    CoffeeWithHook coffeeHook=new CoffeeWithHook();
+	    coffeeHook.prepareRecipe();
+	}
+
+}
